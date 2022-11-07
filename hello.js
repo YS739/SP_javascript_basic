f275e56973b6091613853d87e73c11631914defb
YS739 <suk921@gmail.com>
2022-11-07 11:40:49 +0900

    js class

---
+++ hello.js
@@ -0,0 +1,2 @@
+console.log('Hello world!!')
+// 아아니냐냐냐뇨뇨뇨뉴뉴뉴뉴뉴ㅠ
---
+++ variable.js
@@ -0,0 +1,38 @@
+// let name = 'Yunsuk'
+// console.log(name)
+
+// name = 'Jeong'
+// console.log(name)
+
+// const name = 'Yunsuk'
+// console.log(name)
+
+// name = 'yunny'
+
+// console.log(10)
+
+// let age = 37
+// console.log(age)
+
+// let name = 'yunsuk'
+// console.log(name)
+
+// let isWoman = true
+// let isMan = false
+// console.log(isWoman)
+// console.log(isMan)
+
+// console.log(true)
+// console.log(false)
+
+// let name = null
+// console.log(name)
+
+// let age
+// console.log(age)
+
+// 내가 아무 변수, 상수 지정해서 출력해보기
+
+let age = 47
+console.log(age)
+
